06d405bbde10df2ec0178a943513f21812491dcd
Raghavendra Nagaraja Rao <raghu@Raghavendras-MacBook-Pro.local>
2015-09-13 20:52:52 +0530

    Third commmit

--- plot4.R
+++ plot4.R
@@ -3,7 +3,7 @@ hpc$Date <- as.Date(hpc$Date,format = "%d/%m/%Y")
 hpc_feb<-hpc[hpc$Date == "2007-02-01" | hpc$Date == "2007-02-02",]
 
 hpc_feb$Global_active_power<-as.numeric(hpc_feb$Global_active_power)
-hpc_feb$Global_active_power<-as.numeric(hpc_feb$Voltage)
+hpc_feb$Voltage<-as.numeric(hpc_feb$Voltage)
 hpc_feb$Sub_metering_1<-as.numeric(hpc_feb$Sub_metering_1)
 hpc_feb$Sub_metering_2<-as.numeric(hpc_feb$Sub_metering_2)
 hpc_feb$Sub_metering_3<-as.numeric(hpc_feb$Sub_metering_3)
